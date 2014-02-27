394f620bf7ef3ba411b966f8cbc3e5d66337db89
Chris Buryta <chris@buryta.com>
2014-02-27 09:43:30 -0500

    add install script for yum

---
+++ install-yum.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# Install the dotfiles for the dev server
+#
+
+yum install wget vim -y
+
+if [ ! test -f ~/.gitconfig ]
+    then
+        wget https://raw.github.com/cburyta/dotfiles-server/master/gitconfig -O ~/.gitconfig
+fi
+
+if [ ! test -f ~/.vimrc ]
+    then
+        wget https://raw.github.com/cburyta/dotfiles-server/master/gitconfig -O ~/.vimrc
+fi
